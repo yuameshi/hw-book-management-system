0bf5d4ae2a7533a5c83bc769104614e7b0d14a79
糸 <64469437+yuameshi@users.noreply.github.com>
2024-12-23 11:21:52 +0800

    feat: add wrapper for jdbc

---
+++ src/com/example/book/db/Utils.java
@@ -0,0 +1,65 @@
+package com.example.book.db;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class Utils {
+	private static DataSource dataSource = null;
+
+	static {
+		try {
+			Properties prop = new Properties();
+			prop.load(new FileInputStream("src/druid.properties"));
+			dataSource = DruidDataSourceFactory.createDataSource(prop);
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to initialize druid connection pool.");
+		}
+	}
+
+	public enum Errors {
+		Failed,
+		FailedToConnect,
+		FailedToExecuteQuery,
+	}
+
+	public static DataSource getDataSource() {
+		return dataSource;
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return dataSource.getConnection();
+	}
+
+	public static ResultSet Query(String sql, Statement statement) throws SQLException {
+		ResultSet resultSet = null;
+		boolean hasResultSer = statement.execute(sql);
+		if (hasResultSer) {
+			resultSet = statement.getResultSet();
+			return resultSet;
+		} else {
+			return null;
+		}
+	}
+
+	public static void CloseConnection(ResultSet resultSet, Statement statement, Connection connection)
+			throws SQLException {
+		// 关闭ResultSet
+		if (resultSet != null) {
+			resultSet.close();
+		}
+		// 关闭Statement
+		if (statement != null) {
+			statement.close();
+		}
+		// 关闭Connection
+		if (connection != null) {
+			connection.close();
+		}
+	}
+}
